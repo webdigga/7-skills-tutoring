2abb66ed64386becfe7beda54af4f192956ffc39
webdigga <webdigga42@gmail.com>
2025-01-23 09:31:44 +0000

    Fixing call me button behaviour

--- src/_includes/static/js/callMeButtonBehaviour/callMeButtonBehaviour.js
+++ src/_includes/static/js/callMeButtonBehaviour/callMeButtonBehaviour.js
@@ -5,17 +5,14 @@ function callMeButtonBehaviour() {
 
   
   document.querySelector('[data-test="callMeButton"]').addEventListener("click", (event) => {
-
     event.preventDefault();
-
-    console.log(window.location.href);
-
-    
     window.location.href = /Mobi|Android|iPhone/i.test(navigator.userAgent) ? event.target.href : '/contact/';
   });
 }
 
-callMeButtonBehaviour();
+if(document.querySelector('[data-test="callMeButton"]')) {
+  callMeButtonBehaviour();
+}
 
 if (typeof module === 'object') {
   module.exports = callMeButtonBehaviour;
